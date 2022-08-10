a784af7c8d37f8300cb7787d6ccccb2edcca3ace
dyerlab <dyerlab@icloud.com>
2022-08-10 16:43:45 -0400

    Adding slides and package list

---
+++ StartingPackages.R
@@ -0,0 +1,10 @@
+
+
+# These are the first set of code you'll be seeing this semester.  
+
+#  The following lines of code will install some of the most basic packages we will be using this semester.
+
+install.packages("tidyr")
+install.packages("tidyverse")
+
+
