6911a4d92af641df95f57d3d4e31a3fdbade895d
dyerlab <dyerlab@icloud.com>
2022-08-11 11:54:55 -0400

    adding packages and working on slides

--- StartingPackages.R
+++ StartingPackages.R
@@ -2,8 +2,15 @@
 
 # These are the first set of code you'll be seeing this semester.  
 
-#  The following lines of code will install some of the most basic packages we will be using this semester.
-install.packages("tidyr")
-install.packages("tidyverse")
+#  The following is a collection (the c() part) of library names 
+#    that we will need for this class.  
+package_list <- c( 
+  "tidyverse", "gt", "gtExtras", "reactable",
+  "quarto", "rmarkdown", "gtsummary", 
+  "sf", "skimr", "tidyr" 
+  )
+
+# This function installs each of these packages on your machine.
+install.packages( package_list, type="binary" )
 
 
