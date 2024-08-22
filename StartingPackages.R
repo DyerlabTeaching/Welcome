cb35bb5fc0e748c90f579fdaf2b5672d9ff5a114
Rodney Dyer <rodney@dyerlab.com>
2024-08-22 15:13:24 -0400

    Updating versions for download.

--- StartingPackages.R
+++ StartingPackages.R
@@ -7,10 +7,20 @@
 package_list <- c( 
   "tidyverse", "gt", "gtExtras", "reactable",
   "quarto", "rmarkdown", "gtsummary", 
-  "sf", "skimr", "tidyr" 
+  "sf", "skimr", "tidyr", "knitr", "kableExtra", "tidyterra",
+  "broom", "datatable", "emoji", "wesanderson",
+  "maps", "leaflet", "leaflet.extras", "leaflet.minicharts", "leaflet.providers" 
   )
 
+
+# determine what you already have and then just send the missing ones.
+pkgs <- installed.packages()
+pkgs <- names( is.na(pkgs[,4]))
+
+# 
+to_install <- setdiff( package_list, pkgs )
+
 # This function installs each of these packages on your machine.
-install.packages( package_list, type="binary" )
+install.packages( to_install, type="binary" )
 
 
